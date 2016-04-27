92cc13c6cc9355c3d807b5206e63b8da888d9b0a
koleslena <koleslena@yandex.ru>
2016-04-27 15:33:49 +0700

    change invoke on run

--- xUnit.py
+++ xUnit.py
@@ -3,8 +3,10 @@ class WasRun:
         self.wasRun=None
     def testMethod(self):
         self.wasRun=1
+    def run(self):
+        self.testMethod()
 
 test=WasRun("testMethod")
 print test.wasRun
-test.testMethod()
+test.run()
 print test.wasRun
