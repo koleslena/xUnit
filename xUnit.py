1e1796f75d7d65828362c82e8f984334fc627bad
koleslena <koleslena@yandex.ru>
2016-04-27 16:15:15 +0700

    tearDown green

--- xUnit.py
+++ xUnit.py
@@ -3,10 +3,13 @@ class TestCase:
         self.name=name
     def setUp(self):
         pass
+    def tearDown(self):
+        pass
     def run(self):
         self.setUp()
         method=getattr(self, self.name)
         method()
+        self.tearDown()
 
 class WasRun(TestCase):
     def __init__(self, name):
@@ -15,6 +18,8 @@ class WasRun(TestCase):
         self.log="setUp "
     def testMethod(self):
         self.log=self.log + "testMethod "
+    def tearDown(self):
+        self.log=self.log + "tearDown "
 
 class TestCaseTest(TestCase):
     def testTemplateMethod(self):
