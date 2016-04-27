1430c79775aa6f8129e969e89c35e98b8ed4b63c
koleslena <koleslena@yandex.ru>
2016-04-27 15:52:45 +0700

    setUp green

--- xUnit.py
+++ xUnit.py
@@ -1,7 +1,10 @@
 class TestCase:
     def __init__(self, name):
-        self.name=name  
+        self.name=name
+    def setUp(self):
+        pass
     def run(self):
+        self.setUp()
         method=getattr(self, self.name)
         method()
 
@@ -9,6 +12,8 @@ class WasRun(TestCase):
     def __init__(self, name):
         self.wasRun=None
         TestCase.__init__(self, name)
+    def setUp(self):
+        self.wasSetUp=1
     def testMethod(self):
         self.wasRun=1
 
